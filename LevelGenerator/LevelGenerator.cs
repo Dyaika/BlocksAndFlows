46ef70bb3ae671c9aaa9c3eef17920c6855045f4
Homka <razchurik@gmail.com>
2025-04-04 15:37:03 +0300

    feat(LevelGenerator): generate game matrix with separated blocks

--- LevelGenerator/LevelGenerator.cs
+++ LevelGenerator/LevelGenerator.cs
@@ -9,32 +9,27 @@ public class LevelGenerator: ILevelGenerator
         byte colors = ILevelGenerator.DEFAULT_COLORS_COUNT,
         byte shapes = ILevelGenerator.DEFAULT_SHAPES_COUNT)
     {
-        Filter[,] matrix = new Filter[width, height];
-        var rand = new Random();
-        byte[] buffer;
-        
+        var matrix = new Filter?[width, height];
+
         // Mark filters to make holes
-        int i;
-        for (i = 0; i < width * height / FILTERS_HOLES_RATIO; i++)
-        {
-            var pos = rand.Next() % (width * (height - 2)) + width;
-            matrix[pos / width, pos % width] = new Filter(colorId: colors, shapeId: shapes);
-        }
+        MakeHoles(width, height, colors, shapes, matrix);
 
         PrintMatrix(matrix); // delete this
         
         // Bottom row, both color and shape are completely random
-        for (i = 0; i < width; i++)
-        {
-            buffer = new byte[2];
-            rand.NextBytes(buffer);
-            matrix[i, 0] = new Filter(colorId: (byte)(buffer[0] / colors), shapeId: (byte)(buffer[1] / shapes));
-            
-            // Top row is used as mask to remember previous filters through spaces, it will be changed later
-            matrix[i, height-1] = new Filter(matrix[i, 0]);
-        }
+        GenerateBottomRow(width, height, colors, shapes, matrix);
 
         // Middle rows
+        GenerateMiddleRows(width, height);
+
+        // Top row
+        GenerateTopRow(width, height, colors, shapes, matrix);
+
+        throw new NotImplementedException();
+    }
+
+    private void GenerateMiddleRows(int width, int height)
+    {
         for (var row = 1; row < height - 1; row++)
         {
             for (var col = 0; col < width; col++)
@@ -42,17 +37,64 @@ public class LevelGenerator: ILevelGenerator
                 // TODO: fill mid layers
             }
         }
-        
-        // Top row
-        for (var col = 0; col < width; col++)
-        {
-            // TODO: fill top layer
-        }
-
-        throw new NotImplementedException();
     }
 
-    private void PrintMatrix(Filter[,] matrix)
+    private void GenerateTopRow(int width, int height, byte colors, byte shapes, Filter?[,] matrix)
+    {
+        for (var col = 0; col < width; col++)
+        {
+            matrix[col, height - 1] = GenerateMatchingFilter(matrix[col, height - 1], colors, shapes);
+        }
+    }
+
+    private void GenerateBottomRow(int width, int height, byte colors, byte shapes, Filter?[,] matrix)
+    {
+        int i;
+        var rand = new Random();
+        for (i = 0; i < width; i++)
+        {
+            var buffer = new byte[2];
+            rand.NextBytes(buffer);
+            matrix[i, 0] = new Filter(colorId: (byte)(buffer[0] / colors), shapeId: (byte)(buffer[1] / shapes));
+
+            // Top row is used as mask to remember previous filters through spaces, it will be changed later
+            matrix[i, height - 1] = new Filter(matrix[i, 0]);
+        }
+    }
+
+    private void MakeHoles(int width, int height, byte colors, byte shapes, Filter?[,] matrix)
+    {
+        // initialise all filters with temporary values
+        for (var row = 0; row < height; row++)
+        {
+            for (var col = 0; col < width; col++)
+            {
+                matrix[col, row] = new Filter(0, 0);
+            }
+        }
+        var rand = new Random();
+        for (var i = 0; i < width * height / FILTERS_HOLES_RATIO; i++)
+        {
+            var pos = rand.Next() % (width * (height - 2)) + width;
+            matrix[pos / width, pos % width] = null;
+        }
+    }
+
+    private Filter? GenerateMatchingFilter(Filter? prev, byte colors, byte shapes)
+    {
+        var rand = new Random();
+        var buffer = new byte[2];
+        if (buffer[0] % 2 == 0)
+        {
+            // save shape, change color
+            return new Filter(colorId: (byte)(buffer[1] % colors), shapeId: prev.ShapeId);
+        }
+
+        // save color, change shape
+        return new Filter(colorId: prev.ColorId, shapeId: (byte)(buffer[1] % shapes));
+    }
+
+    private void PrintMatrix(Filter?[,] matrix)
     {
         for (var row = 0; row < matrix.GetLength(0); row++)
         {
