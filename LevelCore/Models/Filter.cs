e57eabe1a4866c21cf503ee13543d75bfdd90cd3
Homka <razchurik@gmail.com>
2025-04-10 16:27:53 +0300

    core(Level): filters now can be broken

--- LevelCore/Models/Filter.cs
+++ LevelCore/Models/Filter.cs
@@ -14,17 +14,40 @@ namespace LevelCore.Models
         /// Shape identificator
         /// </summary>
         public byte ShapeId { get; }
+        
+        /// <summary>
+        /// Checks if filter is broken
+        /// </summary>
+        public bool IsBroken { get; private set; }
+
+        /// <summary>
+        /// Breaks filter
+        /// </summary>
+        public void Break()
+        {
+            IsBroken = true;
+        }
+
+        /// <summary>
+        /// Fixes filter
+        /// </summary>
+        public void Fix()
+        {
+            IsBroken = false;
+        }
 
         public Filter(byte colorId, byte shapeId)
         {
             ColorId = colorId;
             ShapeId = shapeId;
+            IsBroken = false;
         }
 
         public Filter(Filter other)
         {
             ColorId = other.ColorId;
             ShapeId = other.ShapeId;
+            IsBroken = other.IsBroken;
         }
     }
 }
