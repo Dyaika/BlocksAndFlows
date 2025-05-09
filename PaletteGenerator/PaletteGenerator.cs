0ae11b7019ecb7b4087ffdfc63a2660b1c588e1f
Homka <razchurik@gmail.com>
2025-05-10 00:50:23 +0300

    feat(PaletteGenerator): replace kmedoids with repulsion relaxation in CIELab Space

--- PaletteGenerator/PaletteGenerator.cs
+++ PaletteGenerator/PaletteGenerator.cs
@@ -32,9 +32,13 @@ namespace PaletteGenerator
                 case ColorBlindnessType.Monochromacy:
                     return GenerateMonochromacyPalette(n);
                 case ColorBlindnessType.None:
+                    return GenerateRepelledPoints(n, DefaultCIELabDistance);
                 case ColorBlindnessType.Protanopy:
+                    return GenerateRepelledPoints(n, ProtanopyDistance);
                 case ColorBlindnessType.Deuteranopy:
+                    return GenerateRepelledPoints(n, DeuteranopyDistance);
                 case ColorBlindnessType.Tritanopy:
+                    return GenerateRepelledPoints(n, TritanopyDistance);
                 default:
                     return GenerateChromaticPalette(n, type);
             }
@@ -42,7 +46,6 @@ namespace PaletteGenerator
 
         private string[] GenerateChromaticPalette(int n, ColorBlindnessType type = ColorBlindnessType.None)
         {
-            var palette = new List<CIELabColor>();
             var samples = new List<CIELabColor>();
             var random = new Random();
 
@@ -74,9 +77,6 @@ namespace PaletteGenerator
 
                 samples.Add(color);
             }
-
-            if (samples.Count == 0)
-                throw new Exception("No valid samples found!");
 
             var centroids = samples.OrderBy(x => random.Next()).Take(n).ToList();
             var clusters = new List<CIELabColor>[n];
@@ -162,6 +162,90 @@ namespace PaletteGenerator
             return palette;
         }
 
+        public string[] GenerateRepelledPoints(
+            int n,
+            Func<CIELabColor, CIELabColor, double> distanceFunc,
+            int iterations = 100,
+            double stepSize = 1.0,
+            double boundaryRepel = 10.0)
+        {
+            var rand = new Random();
+            var points = new List<CIELabColor>();
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                double l = CIELabColor.MinL + rand.NextDouble() * (CIELabColor.MaxL - CIELabColor.MinL);
+                double a = CIELabColor.MinA + rand.NextDouble() * (CIELabColor.MaxA - CIELabColor.MinA);
+                double b = CIELabColor.MinB + rand.NextDouble() * (CIELabColor.MaxB - CIELabColor.MinB);
+                
+                points.Add(new CIELabColor(l, a, b));
+            }
+
+            for (int iter = 0; iter < iterations; iter++)
+            {
+                var deltas = new (double dL, double dA, double dB)[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    double dL = 0, dA = 0, dB = 0;
+                    var pi = points[i];
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == j) continue;
+                        var pj = points[j];
+
+                        double dist = distanceFunc(pi, pj);
+                        double dx = pi.L - pj.L;
+                        double dy = pi.A - pj.A;
+                        double dz = pi.B - pj.B;
+
+                        double distSq = dist * dist + 1e-6;
+                        double force = 1.0 / distSq;
+
+                        dL += dx * force;
+                        dA += dy * force;
+                        dB += dz * force;
+                    }
+
+                    dL += (pi.L < 5 ? boundaryRepel : 0) - (pi.L > 95 ? boundaryRepel : 0);
+                    dA += (pi.A < -120 ? boundaryRepel : 0) - (pi.A > 120 ? boundaryRepel : 0);
+                    dB += (pi.B < -120 ? boundaryRepel : 0) - (pi.B > 120 ? boundaryRepel : 0);
+
+                    deltas[i] = (dL, dA, dB);
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    var p = points[i];
+                    var (dL, dA, dB) = deltas[i];
+
+                    points[i] = new CIELabColor
+                    (
+                        Clamp(p.L + dL * stepSize, 0, 100),
+                        Clamp(p.A + dA * stepSize, -128, 127),
+                        Clamp(p.B + dB * stepSize, -128, 127)
+                    );
+                }
+            }
+
+            
+            var hexPalette = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                var rgb = _colorConverter.CIELabToSRGB(points[i]);
+                hexPalette[i] = _colorConverter.sRGBToHex(rgb);
+            }
+
+            return hexPalette;
+        }
+
+        private static double Clamp(double x, double min, double max)
+        {
+            return Math.Max(min, Math.Min(max, x));
+        }
+
         private double DefaultCIELabDistance(CIELabColor c1, CIELabColor c2)
         {
             return Math.Sqrt(
@@ -169,6 +253,25 @@ namespace PaletteGenerator
                 Math.Pow(c1.A - c2.A, 2) +
                 Math.Pow(c1.B - c2.B, 2)
             );
+        }
+        
+        private double ProtanopyDistance(CIELabColor c1, CIELabColor c2)
+        {
+            c1 = _blindnessSimulator.SimulateProtonapy(c1);
+            c2 = _blindnessSimulator.SimulateProtonapy(c2);
+            return DefaultCIELabDistance(c1, c2);
+        }
+        private double DeuteranopyDistance(CIELabColor c1, CIELabColor c2)
+        {
+            c1 = _blindnessSimulator.SimulateDeuteranopy(c1);
+            c2 = _blindnessSimulator.SimulateDeuteranopy(c2);
+            return DefaultCIELabDistance(c1, c2);
+        }
+        private double TritanopyDistance(CIELabColor c1, CIELabColor c2)
+        {
+            c1 = _blindnessSimulator.SimulateTritanopy(c1);
+            c2 = _blindnessSimulator.SimulateTritanopy(c2);
+            return DefaultCIELabDistance(c1, c2);
         }
     }
 }
